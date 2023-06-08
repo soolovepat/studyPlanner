18a999245555af2a43e9875eb423d63c74a9fa58
mmms2000 <moseskmcmmms@gmail.com>
2023-06-08 19:31:10 +0900

    로그인 회원가입 수정

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask,session, redirect, url_for, render_template, request, jsonify,flash
 app = Flask(__name__)
 
 
 import datetime as dt
 #test 용입니다.
+# from pymongo import MongoClient
+# client = MongoClient('mongodb+srv://sparta:test@cluster0.efcklz9.mongodb.net/?retryWrites=true&w=majority')
+# db = client.dbsparta
+
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.efcklz9.mongodb.net/?retryWrites=true&w=majority')
+import certifi  #테스트 몽고 
+ca = certifi.where()
+client = MongoClient('mongodb+srv://sparta:test@cluster0.ni7z7tt.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
 
 # DB 저장
 @app.route("/study_planner", methods=["POST"])
@@ -64,8 +71,57 @@ def date_time():
 
     return jsonify({'date':date_receive},{'dayofweek':date_DayOfWeek})
 
-if __name__ == '__main__':
+#로그인 회원가입
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SESSION_TYPE'] = 'filesystem'
 
+users_collection = db['users']
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = users_collection.find_one({'username': username})
+
+        if user and user['password'] == password:
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+# 회원가입
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing_user = users_collection.find_one({'username': username})
+
+        if existing_user:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+
+        new_user = {'username': username, 'password': password}
+        users_collection.insert_one(new_user)
+
+        flash('Registration successful. Please log in.')
+        return redirect(url_for('login'))
+
+    return render_template('register1.html')
+
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
 
     # 테스트입니다 :)
