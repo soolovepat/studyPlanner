5cbd60ff0ef5aa15e134bc81ec8baee9d390f886
soolovepat <soolovepat@naver.com>
2023-06-09 03:23:33 +0900

    페이지 분할

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
+app.config['SERVER_NAME'] = 'localhost:5000'
+app.config['APPLICATION_ROOT'] = '/'  # 애플리케이션의 루트 경로를 설정합니다.
+app.config['PREFERRED_URL_SCHEME'] = 'http'  # URL 스킴을 설정합니다.
 
+import threading
+import time
 
 import datetime as dt
 #test 용입니다.
@@ -64,8 +69,49 @@ def date_time():
 
     return jsonify({'date':date_receive},{'dayofweek':date_DayOfWeek})
 
+
+
+# 서브페이지 - 데이터 저장
+@app.route("/input_form", methods=["GET", "POST"])
+def input_form():
+    if request.method == "POST":
+        group_receive = request.form['group_give']
+        plan_receive = request.form['plan_give']
+        plan_list = list(db.study_planner.find({}, {'_id': False}))
+        count = len(plan_list) + 1
+        
+        # MongoDB에 데이터 저장
+        doc = {
+        'group':group_receive,
+        'plan':plan_receive,
+        'num': count,
+        'done': 0
+        }
+        db.study_planner.insert_one(doc)
+
+        # 알림 스레드 시작
+        t = threading.Thread(target=show_notification)
+        t.start()
+        
+        return jsonify({"msg": "저장 완료!"})
+            
+    return render_template("input_form.html")
+
+def show_notification():
+    time.sleep(5)
+    with app.app_context():
+        return redirect(url_for("form_view"))
+
+   
+# 서브페이지 - 데이터 조회
+@app.route('/form_view')
+def form_view():
+    data = list(db.study_planner.find({}, {'_id': False}))
+    return render_template('form_view.html', result=data)
+    
+
+
+
 if __name__ == '__main__':
 
     app.run('0.0.0.0', port=5000, debug=True)
-
-    # 테스트입니다 :)
