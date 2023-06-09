733650645a7ec35762baab19d9378f30eb3ec663
powerlife145@gmail.com <jinuicode0407!>
2023-06-09 09:08:08 +0900

    날짜 정도 함

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+from bs4 import BeautifulSoup as bs
+from pprint import pprint
+import requests
+
+
+
 app = Flask(__name__)
 
 # import datetime as dt
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.efcklz9.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://powerlife145:test@cluster0.yg0ur8n.mongodb.net/')
 db = client.dbsparta
 
 @app.route('/')
@@ -41,20 +47,21 @@ def plan_undo():
     db.study_planner.update_one({'num':int(num_receive)},{'$set':{'done':0}})
     return jsonify({'msg': '취소 완료!'})
 
-
-
 # DB가져오기
 @app.route("/study_planner", methods=["GET"])
 def plan_get():
     all_plans = list(db.study_planner.find({},{'_id':False}))
     return jsonify({'result': all_plans})
 
+#---------------------------------------------D-DAY,날씨 플랜 DB
 # dday plan DB post(save)
 @app.route("/study_ddayplan", methods=["POST"])
 def ddayplan_post():
     dday_plan = request.form['ddayplan_give']
+    dday_input = request.form['ddayinput_give']
     doc = {
-        'ddplan': dday_plan
+        'ddplan': dday_plan,
+        'd-day' : dday_input
     }
     db.dday_plan.insert_one(doc)
     return jsonify({'msg': 'dday 저장 완료!'})
@@ -62,8 +69,19 @@ def ddayplan_post():
 # dday plan DB get
 @app.route("/study_ddayplan", methods=["GET"])
 def ddayplan_get():
-    all_plans = list(db.study_planner.find({},{'_id':False}))
-    return jsonify({'result': all_plans})
+    ddplans = list(db.dday_plan.find({},{'_id':False}))
+    return jsonify({'ddayResult': ddplans})
+#---------------------------------------------D-DAY 플랜 DB END
+
+@app.route("/study_ddayplan", methods=["GET"])
+def weather_get():
+    html = requests.get('https://search.naver.com/search.naver?query=날씨')
+    soup = bs(html.text,'html.parser')
+
+    data1 = soup.find('span',{'class':'blind'})
+
+    
+    return jsonify({'weatherResult': data1})
 
 if __name__ == '__main__':
 
