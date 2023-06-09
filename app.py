7184eda86bcb68c567160331669b99dc328a0216
mmms2000 <moseskmcmmms@gmail.com>
2023-06-09 14:48:37 +0900

    사용자별 페이지표시

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask,session, redirect, url_for, render_template, request, jsonify,flash
-app = Flask(__name__)
-app.config['SERVER_NAME'] = 'localhost:5000'
-app.config['APPLICATION_ROOT'] = '/'  # 애플리케이션의 루트 경로를 설정합니다.
-app.config['PREFERRED_URL_SCHEME'] = 'http'  # URL 스킴을 설정합니다.
-
+from flask import Flask, session, redirect, url_for, render_template, request, jsonify, flash
 import threading
 import time
-
 import datetime as dt
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.efcklz9.mongodb.net/?retryWrites=true&w=majority')
+
+app = Flask(__name__)
+app.config['SERVER_NAME'] = 'localhost:5001'
+app.config['APPLICATION_ROOT'] = '/'
+app.config['PREFERRED_URL_SCHEME'] = 'http'
+
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://sparta:test@cluster0.ni7z7tt.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
-
+#처음 화면
 @app.route('/')
 def index():
     if 'username' in session:
@@ -25,20 +28,26 @@ def index():
 # DB 저장
 @app.route("/study_planner", methods=["POST"])
 def plan_post():
-    group_receive = request.form['group_give']
-    plan_receive = request.form['plan_give']
-    plan_list = list(db.study_planner.find({}, {'_id': False}))
-    count = len(plan_list) + 1
-      
-    doc = {
-        'group':group_receive,
-        'plan':plan_receive,
-        'num': count,
-        'done': 0
-    }
+    if 'username' in session:
+        username = session['username']
+        group_receive = request.form['group_give']
+        plan_receive = request.form['plan_give']
+        plan_list = list(db.study_planner.find({'username': username}, {'_id': False}))
+        count = len(plan_list) + 1
 
-    db.study_planner.insert_one(doc)
-    return jsonify({'msg': '저장 완료!'})
+        doc = {
+            'username': username,
+            'group': group_receive,
+            'plan': plan_receive,
+            'num': count,
+            'done': 0
+        }
+
+        db.study_planner.insert_one(doc)
+        return jsonify({'msg': '저장 완료!'})
+    else:
+        return jsonify({'msg': '로그인이 필요합니다.'})
+
 
 #DB 수정
 @app.route("/study_planner/done", methods=["POST"])
@@ -58,8 +67,13 @@ def plan_undo():
 # DB가져오기
 @app.route("/study_planner", methods=["GET"])
 def plan_get():
-    all_plans = list(db.study_planner.find({},{'_id':False}))
-    return jsonify({'result': all_plans})
+    if 'username' in session:
+        username = session['username']
+        all_plans = list(db.study_planner.find({'username': username}, {'_id': False}))
+        print(all_plans)
+        return jsonify({'result': all_plans})
+    else:
+        return jsonify({'msg': '로그인이 필요합니다.'})
 
 # 날짜
 @app.route("/datedata")
@@ -80,6 +94,7 @@ def date_time():
 @app.route("/input_form", methods=["GET", "POST"])
 def input_form():
     if request.method == "POST":
+        username = session['username']
         group_receive = request.form['group_give']
         plan_receive = request.form['plan_give']
         plan_list = list(db.study_planner.find({}, {'_id': False}))
@@ -87,10 +102,12 @@ def input_form():
         
         # MongoDB에 데이터 저장
         doc = {
+        'username': username,
         'group':group_receive,
         'plan':plan_receive,
         'num': count,
         'done': 0
+        
         }
         db.study_planner.insert_one(doc)
 
@@ -115,9 +132,6 @@ def form_view():
     return render_template('form_view.html', result=data)
     
 #로그인 회원가입
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['SESSION_TYPE'] = 'filesystem'
-
 users_collection = db['users']
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -166,5 +180,6 @@ def logout():
 
 
 if __name__ == '__main__':
-    
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run('0.0.0.0', port=5001, debug=True)
