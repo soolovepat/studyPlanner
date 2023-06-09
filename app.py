7e77df10286303b9283b5aadcdf680d123ed1fe5
mmms2000 <moseskmcmmms@gmail.com>
2023-06-09 09:52:15 +0900

    수수수정

--- app.py
+++ app.py
@@ -123,10 +123,6 @@ def register():
 
 
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
